6c00616caecbb653bbb7dd7967e7c60afd1acbcc
WilliamJagerDEV <williamjager.dev@gmail.com>
2022-10-23 21:18:23 -0300

    lendo arquivo csv

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -8,13 +8,57 @@ defmodule ReportsGenerator do
 
   ## Examples
 
-      iex> ReportsGenerator.hello()
+      iex> ReportsGenerator.build("report_test.csv")
       :world
 
   """
 
-  # ReportsGenerator.hello
-  def hello do
-    :world
+  # USAR COMANDO "chcp 65001" NO VSCODE PARA UTF8 NO TERMINAL CASO NÃO ESTEJA
+
+  # ===========================================================
+  def build(filename) do
+    # CASE MODE
+    # case file = File.read("reports/#{filename}") do
+    #   {:ok, result} -> result
+    #   {:error, reason} -> reason
+    # end
+
+    #PIPE & OVERLOAD MODE
+    "reports/#{filename}"
+    |> File.read()
+    |> handle_file("Arquivo: ")
+
   end
+
+  # def handle_file({:ok, result}, msg), do: msg <> result
+  def handle_file({:ok, result}, msg) do
+    msg <> result
+  end
+
+  # def handle_file({:error, reason}, msg), do: msg <> "erro: " <> reason
+  def handle_file({:error, reason}, msg) do
+    msg <> "erro: " <> "#{reason}"
+  end
+
+  # ===========================================================
+
+  # ReportsGenerator.start("report_test.csv")
+  def start(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    # |> Enum.each(fn line -> IO.inspect(line) end)
+    |> Enum.map(fn line -> parse_line(line) end)
+  end
+
+  def parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    # FUNÇÃO ANÔNIMA IMPLÍCITA COM & E /1
+    |> List.update_at(2, &String.to_integer/1)
+  end
+
+
+
+
 end
