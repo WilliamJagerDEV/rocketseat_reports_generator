18645d5c0d7f72675d29ec7d06977f5fdbedaac8
WilliamJagerDEV <williamjager.dev@gmail.com>
2022-10-26 22:51:06 -0300

    falha ao criar teste de caso de erro

--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -2,7 +2,7 @@ defmodule ReportsGeneratorTest do
   use ExUnit.Case
 
   describe "start/1" do
-    test "builds do report" do
+    test "builds the report" do
       # SETUP
       file_name = "report_test.csv"
 
@@ -55,6 +55,71 @@ defmodule ReportsGeneratorTest do
       }
 
       # ASSERTION
+      assert response == expected_response
+    end
+  end
+
+  describe "start_from_many/1" do
+    test "when a file list is provides, builds the report" do
+      # SETUP
+      file_names = ["report_test.csv", "report_test.csv"]
+
+      # EXERCISE
+      response = ReportsGenerator.start_from_many(file_names)
+
+      expected_response = %{
+        "foods" => %{
+          "açaí" => 2,
+          "churrasco" => 4,
+          "esfirra" => 6,
+          "hambúrguer" => 4,
+          "pastel" => 0,
+          "pizza" => 4,
+          "prato_feito" => 0,
+          "sushi" => 0
+        },
+        "users" => %{
+          "1" => 96,
+          "10" => 72,
+          "11" => 0,
+          "12" => 0,
+          "13" => 0,
+          "14" => 0,
+          "15" => 0,
+          "16" => 0,
+          "17" => 0,
+          "18" => 0,
+          "19" => 0,
+          "2" => 90,
+          "20" => 0,
+          "21" => 0,
+          "22" => 0,
+          "23" => 0,
+          "24" => 0,
+          "25" => 0,
+          "26" => 0,
+          "27" => 0,
+          "28" => 0,
+          "29" => 0,
+          "3" => 62,
+          "30" => 0,
+          "4" => 84,
+          "5" => 98,
+          "6" => 36,
+          "7" => 54,
+          "8" => 50,
+          "9" => 48
+        }
+      }
+
+      # ASSERTION
+      assert response == expected_response
+    end
+
+    test "when a file list is not provides, returns an error." do
+      response = ReportsGenerator.start_from_many("banana")
+
+      expected_response = "banana"
       assert response == expected_response
     end
   end
